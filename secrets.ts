dd31f943eedf68da424618c393c37bb0bcc49ef3
Brian Makhembu <31996990+makhembu@users.noreply.github.com>
2026-02-22 21:19:34 +0300

    Update secrets.ts

--- secrets.ts
+++ secrets.ts
@@ -1,9 +1,9 @@
 
 export const SECRETS = {
   // Supabase Configuration
-  SUPABASE_URL: "https://irsurnyfjgjmlhlrkbeh.supabase.co",
-  SUPABASE_PUBLISHABLE_DEFAULT_KEY: "sb_publishable_Zqgj49fvzbeSxzKBaRM38Q_6bLHV2rZ", 
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlyc3VybnlmamdqbWxobHJrYmVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwMTc5MzYsImV4cCI6MjA4NDU5MzkzNn0.rxmPM8x7x1KMAhKjbKk2yh2Unq_PrV9iaDRil6wOZaM",
+  SUPABASE_URL: "",
+  SUPABASE_PUBLISHABLE_DEFAULT_KEY: "", 
+  SUPABASE_ANON_KEY: "",
   // AI Configuration
   GEMINI_API_KEY: "", 
 };
